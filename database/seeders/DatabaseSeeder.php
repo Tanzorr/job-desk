635698394ab8cea090069828f850832a2df6945d
olexandr <alexx1984@ukr.net>
2024-01-29 17:52:17 +0200

    add emploer seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Employer;
 use App\Models\Job;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::factory(300)->create();
+
+        $users = User::all()->shuffle();
+
+        for($i = 1; $i <= 20; $i++) {
+           Employer::factory()->create([
+               'user_id' => $users->pop()->id
+           ]);
+        }
+
+        $employers = Employer::all();
+
+        for($i = 1; $i <= 100; $i++) {
+            Job::factory()->create([
+                'employer_id' => $employers->pop()->id
+            ]);
+        }
+
         Job::factory()->count(100)->create();
         // \App\Models\User::factory(10)->create();
 
